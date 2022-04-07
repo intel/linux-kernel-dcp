93f1f8418772698fd35c70e3192f4339408a4ce6
Kan Liang <kan.liang@linux.intel.com>
2022-04-07 03:26:21 -0700

    perf/x86/intel: Read call chain from CET shadow stack
    (Remove the dependency of __cet_get_shstk_addr().
     The original one can be found in tgl_release branch)
    
    Current call chain in PERF_SAMPLE_CALLCHAIN data format is read from
    frame pointer. However, the frame pointers are off by default on 64bit
    code (and on modern 32bit gccs), so there are many binaries around that
    do not use frame pointers.
    There could be other ways to retrieve the call chain as well, e.g
    dwarf unwinding. But it also only works when the applications are
    correctly annotated everywhere, including any hand written assembler
    code, and it is expensive because it needs to copy the whole stack.
    
    With Control-flow Enforcement Technology, shadow stack can be used as
    another option for accurate call chain.
    
    To apply shadow stack from CET, applications have to be compiled for
    CET, e.g. with gcc option -fcf-protection -mshstk.
    Currently, the Shadow Stack is only enabled for user-mode applications.
    
    Add a knob, cet_shadow_stack_call_chain in sysfs, to enable CET perf
    support. The default value is true. Using the shadow stack to replace
    frame pointer for user-mode applications, if CET is enabled. Otherwise,
    fall back to frame pointer mode.
    
    Optimize for 64 bit process by copying the whole shadow stack in one
    __copy_from_user_nmi() operation.
    
    Current implement is on top of full CET support in binaries for now.
    In theory it might be able to only enable shadow stack. That can be
    done later separately.
    
    Here is an example.
    
     #gcc -o test_cet test_cet.c -fcf-protection -mshstk
      -fomit-frame-pointer
    
    Without CET,
     #echo 0 > /sys/devices/cpu/cet_shadow_stack_call_chain
     #perf record -e cycles:up -g ./test_cet
     #perf report --stdio
     # Samples: 19K of event 'cycles:up'
     # Event count (approx.): 19093119326
     #
     # Children      Self  Command          Shared Object      Symbol
     # ........  ........  ...............  .................
     # ......................
     #
       100.00%     0.00%  test_cet         [unknown]          [.]
     0x49564100002be33d
                |
                ---0x49564100002be33d
                   |
                    --99.84%--f3
    
        99.84%    99.84%  test_cet         test_cet           [.] f3
                |
                ---0x49564100002be33d
                   f3
    
    With CET,
     #echo 1 > /sys/devices/cpu/cet_shadow_stack_call_chain
     #perf record -e cycles:up -g ./test_cet
     #perf report --stdio
     # Children      Self  Command          Shared Object     Symbol
     # ........  ........  ...............  ................
     # ......................
     #
       100.00%     0.00%  test_cet          test_cet      [.] _start
                |
                ---_start
                   __libc_start_main
                   main
                   f1
                   f2
                   |
                    --99.81%--f3
    
    Here is the performance result for 64 bit process.
    
      #gcc -o test_cet_67 test_cet_67.c -fcf-protection -mshstk -m64
      #perf record -e instructions:u --call-graph fp -c10000 ./test_cet_67.c
      (The depth of call chain is 67 for almost all samples.)
    
    FP:               echo 0 > /sys/devices/cpu/cet_shadow_stack_call_chain
    CET shadow stack: echo 1 > /sys/devices/cpu/cet_shadow_stack_call_chain
    
    The latency of perf_callchain_user()(Calculated by ftrace)
                                   Latency (us)
    FP:                            1.84
    CET shadow stack:              0.17
    
    The elapsed time of test_cet_67.
                                   Elapsed time (us)    Overhead
    Baseline (without perf):       26380272
    FP:                            27790702             5.53%
    CET shadow stack:              26967926             2.23%
    
    For 32 bit process, there are no big differences. The performance of CET
    shadow stack is a little bit better than FP.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -2839,6 +2839,10 @@ perf_callchain_user32(struct pt_regs *regs, struct perf_callchain_entry_ctx *ent
 
 	fp = compat_ptr(ss_base + regs->bp);
 	pagefault_disable();
+
+	if (x86_pmu.store_shadow_stack_user && x86_pmu.store_shadow_stack_user(entry))
+		goto out;
+
 	while (entry->nr < entry->max_stack) {
 		if (!valid_user_frame(fp, sizeof(frame)))
 			break;
@@ -2851,6 +2855,7 @@ perf_callchain_user32(struct pt_regs *regs, struct perf_callchain_entry_ctx *ent
 		perf_callchain_store(entry, cs_base + frame.return_address);
 		fp = compat_ptr(ss_base + frame.next_frame);
 	}
+out:
 	pagefault_enable();
 	return 1;
 }
@@ -2890,6 +2895,10 @@ perf_callchain_user(struct perf_callchain_entry_ctx *entry, struct pt_regs *regs
 		return;
 
 	pagefault_disable();
+
+	if (x86_pmu.store_shadow_stack_user && x86_pmu.store_shadow_stack_user(entry))
+		goto out;
+
 	while (entry->nr < entry->max_stack) {
 		if (!valid_user_frame(fp, sizeof(frame)))
 			break;
@@ -2902,6 +2911,7 @@ perf_callchain_user(struct perf_callchain_entry_ctx *entry, struct pt_regs *regs
 		perf_callchain_store(entry, frame.return_address);
 		fp = (void __user *)frame.next_frame;
 	}
+out:
 	pagefault_enable();
 }
 
